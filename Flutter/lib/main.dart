09ca44a105d85cff509e29274dc783e878c4c496
bennywu2019 <benny.wu.new@gmail.com>
2024-01-03 21:20:30 -0500

    reorganize codebase

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+// Custom files
 import 'journal.dart';
 import 'style.dart';
 
@@ -15,26 +17,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromRGBO(134, 177, 9, 1),
-            brightness: Brightness.light
-        ),
-        useMaterial3: true,
-        scaffoldBackgroundColor: bgcolor,
-        fontFamily: GoogleFonts.jost().fontFamily
-      ),
+      theme: lightTheme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-List<String> a = ['a', 'b', 'c'];
-
 /*
   ----------------------- Homepage ----------------------------
  */
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -45,130 +36,150 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   /*
-  The list containing the journals
-  and the function for updating the list
+  The list containing the journals and the function for updating the list
    */
 
+// These dummy data should be in the form of json files (for "fake" fetching).
+// This enables a smoother transition to backend fetching
+
   List<Journal> journalList = [
-    Journal(title: "Hello World", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
-        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
-        "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
-        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
-        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, "
-        "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    Journal(
+        title: "Hello World",
+        content:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+            "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+            "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+            "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+            "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, "
+            "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
     Journal(title: "Benny Orz", content: "je suis un vegetable"),
-    Journal(title: "Dalao nb", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.text ever since the 1500s"),
+    Journal(
+        title: "Dalao nb",
+        content:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry.text ever since the 1500s"),
   ];
 
-  void addJournal(Journal j){
+  void addJournal(Journal j) {
     setState(() {
       journalList.add(j);
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: bgcolor,
-        centerTitle: true,
-        scrolledUnderElevation: 0,
-        title: Image.asset(
-          'assets/images/lp_logo_l.png',
-          scale: 4,
-        )
-      ),
+          backgroundColor: bgcolor,
+          centerTitle: true,
+          scrolledUnderElevation: 0,
+          title: Image.asset(
+            'assets/images/lp_logo_l.png',
+            scale: 4,
+          )),
       body: Stack(
-          children: <Widget>[
-            Padding(
+        children: <Widget>[
+          Padding(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Padding(
-                  padding: EdgeInsets.only(bottom: 100),
-                  child: ListView.builder(
-                    itemCount: journalList.length,
-                    shrinkWrap: false,
-                    itemBuilder: (BuildContext context, int index){
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 0),
-                        child: Card(
-                          color: medgreen,
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 0),
-                            child: ListTile(
-                              title: Text(journalList[index].title, style: whiteSubmenuHeader),
-                              subtitle: Text(
-                                journalList[index].content,
-                                style: const TextStyle(color: Colors.white, fontSize: 12),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 2,
-                              ),
-                              onTap: () {
-                                // Navigate to the journal entry page
-                              },
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 100),
+                child: ListView.builder(
+                  itemCount: journalList.length,
+                  shrinkWrap: false,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 3, horizontal: 0),
+                      child: Card(
+                        color: medgreen,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 6, horizontal: 0),
+                          child: ListTile(
+                            title: Text(journalList[index].title,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall),
+                            textColor: Colors.white,
+                            subtitle: Text(
+                              journalList[index].content,
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 12),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
                             ),
+                            onTap: () {
+                              // Navigate to the journal entry page
+                            },
                           ),
                         ),
-                      );
-                    },
-                  ),
-                )
+                      ),
+                    );
+                  },
+                ),
+              )),
 
-              ),
-
-
-            /*
+          /*
             The bottom navbar consisting of three FABs in a row
              */
-            Positioned(
-              bottom: 30.0,  // Adjust this value to move the buttons upwards
-              left: 0.0,
-              right: 0.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const FloatingActionButton(
-                      onPressed: null,
-                      tooltip: 'Friends',
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      child: Icon(Icons.group, color: dark, size: 30.0,)
-                  ),
-                  const SizedBox(width: 40,),
-                  FloatingActionButton(
-                      onPressed: (){
-                        Navigator.push(
+          Positioned(
+            bottom: 30.0, // Adjust this value to move the buttons upwards
+            left: 0.0,
+            right: 0.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const FloatingActionButton(
+                    onPressed: null,
+                    tooltip: 'Friends',
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    child: Icon(
+                      Icons.group,
+                      color: dark,
+                      size: 30.0,
+                    )),
+                const SizedBox(
+                  width: 40,
+                ),
+                FloatingActionButton(
+                    onPressed: () {
+                      Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => CreatePostPage(addJournalCallback: addJournal))
-                        );
-                      },
-                      tooltip: 'Increment',
-                      backgroundColor: medgreen,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)
-                      ),
-                      child: const Icon(Icons.add, color: Colors.white, size: 35.0,)
-                  ),
-                  const SizedBox(width: 40,),
-                  const FloatingActionButton(
-                      onPressed: null,
-                      tooltip: 'Settings',
-                      elevation: 0,
-                      backgroundColor: Colors.transparent,
-                      child: Icon(Icons.settings, color: dark, size: 30.0,)
-                  ),
-                ],
-              ),
+                          MaterialPageRoute(
+                              builder: (context) => CreatePostPage(
+                                  addJournalCallback: addJournal)));
+                    },
+                    tooltip: 'Increment',
+                    backgroundColor: medgreen,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: const Icon(
+                      Icons.add,
+                      color: Colors.white,
+                      size: 35.0,
+                    )),
+                const SizedBox(
+                  width: 40,
+                ),
+                const FloatingActionButton(
+                    onPressed: null,
+                    tooltip: 'Settings',
+                    elevation: 0,
+                    backgroundColor: Colors.transparent,
+                    child: Icon(
+                      Icons.settings,
+                      color: dark,
+                      size: 30.0,
+                    )),
+              ],
             ),
-          ],
-        ),
-      );
-      // floatingActionButton:
+          ),
+        ],
+      ),
+    );
+    // floatingActionButton:
   }
 }
-
 
 /*
   ----------------------- Post Page ----------------------------
@@ -176,14 +187,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class CreatePostPage extends StatefulWidget {
   final Function(Journal) addJournalCallback;
-  const CreatePostPage({Key? key, required this.addJournalCallback}) : super(key: key);
+  const CreatePostPage({Key? key, required this.addJournalCallback})
+      : super(key: key);
 
   @override
   State<CreatePostPage> createState() => _CreatePostPageState();
 }
 
 class _CreatePostPageState extends State<CreatePostPage> {
-
   final journalTitleController = TextEditingController();
   final journalTextController = TextEditingController();
 
@@ -191,9 +202,9 @@ class _CreatePostPageState extends State<CreatePostPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
+        title: Text(
           'New Journal',
-          style: header
+          style: Theme.of(context).textTheme.headlineMedium,
         ),
         backgroundColor: bgcolor,
         centerTitle: true,
@@ -207,32 +218,30 @@ class _CreatePostPageState extends State<CreatePostPage> {
               padding: const EdgeInsets.only(left: 12),
               // Journal Title
               child: TextFormField(
-                decoration: const InputDecoration(
+                decoration: InputDecoration(
                     labelText: "Journal Title",
                     border: InputBorder.none,
                     hintText: "What's going on in your life?",
-                    hintStyle: hintText,
-                    labelStyle: submenuHeader
-                ),
-                style: submenuHeader,
+                    hintStyle: Theme.of(context).textTheme.labelMedium,
+                    labelStyle: Theme.of(context).textTheme.headlineSmall),
+                style: Theme.of(context).textTheme.headlineSmall,
                 controller: journalTitleController,
               ),
             ),
-
             Container(
               decoration: BoxDecoration(
                 color: lightgreen,
                 borderRadius: BorderRadius.circular(20.0),
               ),
-              padding: const EdgeInsets.only(left: 20, top: 0, right: 20, bottom: 60),
+              padding: const EdgeInsets.only(
+                  left: 20, top: 0, right: 20, bottom: 60),
               // Journal Body
               child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: "How's it going?",
-                  border: InputBorder.none,
-                  labelStyle: hintText
-                ),
-                style: bodyText,
+                decoration: InputDecoration(
+                    labelText: "How's it going?",
+                    border: InputBorder.none,
+                    labelStyle: Theme.of(context).textTheme.labelMedium),
+                style: Theme.of(context).textTheme.bodyMedium,
                 maxLines: null,
                 controller: journalTextController,
               ),
@@ -241,26 +250,25 @@ class _CreatePostPageState extends State<CreatePostPage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          /*
+          onPressed: () {
+            /*
           Post journal
            */
-          widget.addJournalCallback(
-              Journal(
+            widget.addJournalCallback(Journal(
                 title: journalTitleController.text,
-                content: journalTextController.text
-              )
-            );
-          journalTextController.clear();
-          journalTitleController.clear();
-          Navigator.pop(context);
+                content: journalTextController.text));
+            journalTextController.clear();
+            journalTitleController.clear();
+            Navigator.pop(context);
           },
-        tooltip: "Post",
-        backgroundColor: medgreen,
-          child: const Icon(Icons.send, color: Colors.white, size: 26.0,)
-      ),
+          tooltip: "Post",
+          backgroundColor: medgreen,
+          child: const Icon(
+            Icons.send,
+            color: Colors.white,
+            size: 26.0,
+          )),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
-
--- Flutter/lib/style.dart
+++ Flutter/lib/style.dart
@@ -1,38 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+// Chosen by Bill dalao
 const bgcolor = Color.fromRGBO(255, 247, 219, 1);
 const medgreen = Color.fromRGBO(144, 176, 91, 1);
 const dark = Color.fromRGBO(62, 62, 62, 1);
 const darkgreen = Color.fromRGBO(74, 129, 18, 1);
 const lightgreen = Color.fromRGBO(210, 224, 196, 1);
 
-const TextStyle header = TextStyle(
-    fontWeight: FontWeight.w500,
-    fontSize: 24,
-    color: dark
-);
+// header => headlineMedium
+// submenuHeader => headlineSmall
+// whiteSubmenuHeader => headlineSmall (colours can be overwritten right?)
+// hintText => labelMedium
+// bodyText => bodyMedium
 
-const TextStyle submenuHeader = TextStyle(
-    fontWeight: FontWeight.w600,
-    color: darkgreen,
-    fontSize: 20
-);
-
-const TextStyle whiteSubmenuHeader = TextStyle(
-    fontWeight: FontWeight.w600,
-    color: Colors.white,
-    fontSize: 18
-);
-
-const TextStyle hintText = TextStyle(
-    fontWeight: FontWeight.w400,
-    color: Colors.black26,
-    fontSize: 14
-);
-
-const TextStyle bodyText = TextStyle(
-    fontWeight: FontWeight.w400,
-    color: dark,
-    fontSize: 16
-);
+ThemeData lightTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: const Color.fromRGBO(134, 177, 9, 1),
+      brightness: Brightness.light,
+    ),
+    textTheme: const TextTheme(
+        headlineMedium:
+            TextStyle(fontWeight: FontWeight.w500, fontSize: 24, color: dark),
+        headlineSmall: TextStyle(
+            fontWeight: FontWeight.w600, fontSize: 20, color: darkgreen),
+        labelMedium: TextStyle(
+            fontWeight: FontWeight.w400, color: Colors.black26, fontSize: 14),
+        bodyMedium:
+            TextStyle(fontWeight: FontWeight.w400, color: dark, fontSize: 16)),
+    useMaterial3: true,
+    scaffoldBackgroundColor: bgcolor,
+    fontFamily: GoogleFonts.jost().fontFamily);
