e8abe788d0f699e974a853b8fe82bb281251c071
icsbillwei <shuheng.wei@outlook.com>
2024-01-03 19:56:38 -0500

    [BUG] implemented posting page

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -60,12 +60,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  /*
+  The list containing the journals
+  and the function for updating the list
+   */
+
+  List<Journal> journalList = [
+    Journal(title: "Hello World", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+        "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, "
+        "when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+        "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "
+        "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, "
+        "and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    Journal(title: "Benny Orz", content: "je suis un vegetable"),
+    Journal(title: "Dalao nb", content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.text ever since the 1500s"),
+  ];
+
+  void addJournal(Journal j){
+    setState(() {
+      journalList.add(j);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: bgcolor,
         centerTitle: true,
+        scrolledUnderElevation: 0,
         title: Image.asset(
           'assets/images/lp_logo_l.png',
           scale: 4,
@@ -77,19 +102,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Stack(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 25),
-              child: Column(
-                children: <Widget>[
-                  const SizedBox(height: 10,),
-                  ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 100),
+                  child: ListView.builder(
                     itemCount: journalList.length,
+                    shrinkWrap: true,
                     itemBuilder: (BuildContext context, int index){
                       return Padding(
                         padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 0),
                         child: Card(
                           color: medgreen,
                           child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 7, horizontal: 0),
+                            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 0),
                             child: ListTile(
                               title: Text(journalList[index].title, style: whiteSubmenuHeader),
                               subtitle: Text(
@@ -106,11 +131,10 @@ class _MyHomePageState extends State<MyHomePage> {
                         ),
                       );
                     },
-                    shrinkWrap: true,
-                  )
-                ],
+                  ),
+                )
+
               ),
-            ),
 
 
             /*
@@ -123,19 +147,19 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  FloatingActionButton(
+                  const FloatingActionButton(
                       onPressed: null,
                       tooltip: 'Friends',
                       elevation: 0,
                       backgroundColor: Colors.transparent,
-                      child: const Icon(Icons.group, color: dark, size: 30.0,)
+                      child: Icon(Icons.group, color: dark, size: 30.0,)
                   ),
                   const SizedBox(width: 40,),
                   FloatingActionButton(
                       onPressed: (){
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => const CreatePostPage()),
+                          MaterialPageRoute(builder: (context) => CreatePostPage(addJournalCallback: addJournal))
                         );
                       },
                       tooltip: 'Increment',
@@ -146,12 +170,12 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: const Icon(Icons.add, color: Colors.white, size: 35.0,)
                   ),
                   const SizedBox(width: 40,),
-                  FloatingActionButton(
+                  const FloatingActionButton(
                       onPressed: null,
                       tooltip: 'Settings',
                       elevation: 0,
                       backgroundColor: Colors.transparent,
-                      child: const Icon(Icons.settings, color: dark, size: 30.0,)
+                      child: Icon(Icons.settings, color: dark, size: 30.0,)
                   ),
                 ],
               ),
@@ -170,7 +194,8 @@ class _MyHomePageState extends State<MyHomePage> {
  */
 
 class CreatePostPage extends StatefulWidget {
-  const CreatePostPage({Key? key}) : super(key: key);
+  final Function(Journal) addJournalCallback;
+  const CreatePostPage({Key? key, required this.addJournalCallback}) : super(key: key);
 
   @override
   State<CreatePostPage> createState() => _CreatePostPageState();
@@ -178,8 +203,8 @@ class CreatePostPage extends StatefulWidget {
 
 class _CreatePostPageState extends State<CreatePostPage> {
 
-  // final journalTitleController = TextEditingController();
-  // final journalTextController = TextEditingController();
+  final journalTitleController = TextEditingController();
+  final journalTextController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -199,6 +224,7 @@ class _CreatePostPageState extends State<CreatePostPage> {
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.only(left: 12),
+              // Journal Title
               child: TextFormField(
                 decoration: const InputDecoration(
                     labelText: "Journal Title",
@@ -208,6 +234,7 @@ class _CreatePostPageState extends State<CreatePostPage> {
                     labelStyle: submenuHeader
                 ),
                 style: submenuHeader,
+                controller: journalTitleController,
               ),
             ),
 
@@ -217,6 +244,7 @@ class _CreatePostPageState extends State<CreatePostPage> {
                 borderRadius: BorderRadius.circular(20.0),
               ),
               padding: const EdgeInsets.only(left: 20, top: 0, right: 20, bottom: 60),
+              // Journal Body
               child: TextFormField(
                 decoration: const InputDecoration(
                   labelText: "How's it going?",
@@ -225,6 +253,7 @@ class _CreatePostPageState extends State<CreatePostPage> {
                 ),
                 style: bodyText,
                 maxLines: null,
+                controller: journalTextController,
               ),
             )
           ],
@@ -232,8 +261,19 @@ class _CreatePostPageState extends State<CreatePostPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          Navigator.pop(context); // placeholder
-        },
+          /*
+          Post journal
+           */
+          widget.addJournalCallback(
+              Journal(
+                title: journalTitleController.text,
+                content: journalTextController.text
+              )
+            );
+          journalTextController.clear();
+          journalTitleController.clear();
+          Navigator.pop(context);
+          },
         tooltip: "Post",
         backgroundColor: medgreen,
           child: const Icon(Icons.send, color: Colors.white, size: 26.0,)
